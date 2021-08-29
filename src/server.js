f73652821c82e9dea4631324564ba25f6b759f36
Kristijonas Kačiušis <84323029+KKaciusis@users.noreply.github.com>
2021-08-29 20:07:56 +0300

    terminal selection meniu

--- src/server.js
+++ src/server.js
@@ -2,6 +2,14 @@ import readline from "readline";
 import mysql from "mysql";
 
 
+const ACTION_ADD = 1;
+const ACTION_DELETE = 2;
+const ACTION_LIST = 3;
+const ACTION_EDIT = 4;
+const ACTION_FINISH = 0;
+
+let SELECT_ACTION = "";
+
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout,
@@ -47,13 +55,32 @@ function query(conn, sql, params){
         });
     });
 }
+meniu();
+function showInfo() {
+    console.log(`
+        1. Add new cow
+        2. Delete a cow
+        3. List all cows
+        4. Edit cow
+        5. End connection
+    `);
+}
 
+async function meniu(){
+    showInfo();
+    try {
+        SELECT_ACTION = await inputNumber("Choose stuff:  ");
+    } catch(err){
+        console.log("Please input a number");
+    } await main();
+}
+async function addAction(){
 try {
     conn.connect();
-
-    const cowName = await inputText("Please input name: ");
-    const cowNickName = await inputText("Please input nick name:  ");
-    let {results: r, fields: f} = await query(conn, `select * from cow where name like ? and nickname like ?`, [`%${cowName}%`, `%${cowNickName}%`]);
+    const cowName = await inputText("Please input name : ");
+    const cowNickName = await inputText("Please input nick name :  ");
+    const cowMilk = await inputText("please input cow milk production ml/hr :  ");
+    let {results: r, fields: f} = await query(conn, `insert into cow (name, nickname, milkCount) values(?, ?, ?)`, [cowName, cowNickName, cowMilk]);
 
     console.log(f);
     console.log(r);
@@ -61,5 +88,33 @@ try {
     console.log("error", err);
 } finally {
     conn.end();
-    rl.close();
+}
+}
+async function deleteAction(){
+    
+}
+async function listAction() {
+    try{
+        conn.connect();
+        let {results: r, fields: f} = await query(conn, `select * from cow`);
+        console.log(r);
+    } catch(err){
+        console.log("failed to read info");
+    }
+}
+async function main(){
+    if(SELECT_ACTION === ACTION_FINISH){
+        console.log("closing");
+        rl.close
+    } else {
+        if (SELECT_ACTION === ACTION_ADD){
+            await addAction();
+        } else if (SELECT_ACTION === ACTION_DELETE){
+            await deleteAction();
+        } else if (SELECT_ACTION === ACTION_EDIT){
+            await editAction();
+        } else if (SELECT_ACTION === ACTION_LIST){
+            await listAction();
+        }
+    }
 }
