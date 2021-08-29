f4aa4304e02203df324ebd70ad1b05db62ef6f83
Kristijonas Kačiušis <84323029+KKaciusis@users.noreply.github.com>
2021-08-29 14:49:38 +0300

    connection to sql server

--- src/server.js
+++ src/server.js
@@ -2,10 +2,32 @@ import mysql from 'mysql';
 
 const conn = mysql.createConnection({
     host: "localhost",
+    database: "cow_tier_list",
     user: "Bloten",
     password: "milkexpert3000",
 });
 
+function query(conn, sql, params){
+    return new Promise((resolve, reject) =>{
+        conn.query(sql, params, (err, results, fields) =>{
+            if (err){
+               return reject(err);
+            }
+            resolve({
+                results,
+                fields
+            });
+        });
+    });
+}
+
 conn.connect();
 console.log("connected")
+let {results: r, fields: f} = await query(conn, "select * from cow where name='Marge'");
+
+console.log(f);
+console.log(r);
+
+
+
 conn.end();
